1e6b217ef87fe7e67cd0e341f8f15c99d463664f
yousefh409 <yousefh409@gmail.com>
2026-07-20 01:39:45 -0700

    ui: freeze embed prop contracts (VendoAppEmbed/VendoApprovalEmbed/VendoToolResult, types only)

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,6 +3,7 @@ export { createVendoClient, type VendoClient, type VendoClientConfig } from "./c
 export { VendoProvider, hostComponentMap, useVendoContext, useVendoDiscoverability, useVendoGreeting, useVendoTheme, useVendoTools, type ConnectorOption, type HostComponentsInput } from "./context.js";
 export { defaultVendoGreeting, type VendoDiscoverability, type VendoGreeting } from "./chrome/discoverability.js";
 export type { ToolMeta, ToolMetaMap } from "./chrome/humanize.js";
+export type { VendoAppEmbedProps, VendoApprovalEmbedProps, VendoApprovalEmbedState, VendoToolResultProps } from "./embeds.js";
 export * from "./hooks/index.js";
 export { defaultVendoTheme, resolveTheme, themeCssVariables } from "./theme.js";
 export { useVoice, type UseVoiceResult } from "./voice/use-voice.js";
