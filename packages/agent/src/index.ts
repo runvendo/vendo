c34a125f57532054ccc289f8de0e575d72c7ad67
yousefh409 <yousefh409@gmail.com>
2026-07-20 01:38:18 -0700

    agent: freeze the public tool-pack contract (names, options, delegate result)

--- packages/agent/src/index.ts
+++ packages/agent/src/index.ts
@@ -1,5 +1,13 @@
 export { createAgent } from "./agent.js";
 export type { VendoAgent } from "./agent.js";
 export type { CapabilityMissConfig } from "./capability-miss.js";
+export {
+  VENDO_CREATE_APP_TOOL,
+  VENDO_DELEGATE_TOOL,
+  VENDO_TOOL_PACK_PREFIX,
+  type VendoDelegateResult,
+  type VendoToolPackFilter,
+  type VendoToolPackOptions,
+} from "./tool-pack.js";
 export type { ToolSearchConfig, ToolSearchFn, ToolSearchMatch } from "./tool-search.js";
 export type { Thread, ThreadSummary } from "./threads.js";
